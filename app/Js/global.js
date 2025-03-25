e1ace88561549aa15cb6c42bf87feefb33062ec7
kevin1157duarte <kevin1157.kd@gmail.com>
2025-03-25 02:04:45 -0300

    configurei o menu e arrumei bugs
    dei funcionalidade para o btn do menu, arrumei algumas proporçoes e a forma de mudar a pagina

--- app/Js/global.js
+++ app/Js/global.js
@@ -3,10 +3,11 @@
     Componente Barra de Navegação
 ---------------------------------------*/
 class BarraNav extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML =
-            `<nav id="barraDeBusca" class="navbar navbar-light bg-light"> <!--Configuração da Barra de Navegação-->
-    <div id="logo" onclick="">
+  connectedCallback() {
+    this.innerHTML =
+      `<nav id="barraDeBusca" class="navbar navbar-light bg-light"> <!--Configuração da Barra de Navegação-->
+      <div id="btnMenuNav"><span class="material-icons">menu</span></div>
+    <div id="logo">
       <img src="Imagens/ChronoZoneIMG.jpg" alt="">
       <a class="navbar-brand">ChronoZone</a>
     </div>
@@ -19,12 +20,31 @@ class BarraNav extends HTMLElement {
       <img src="Imagens/Sem_Foto_Perfil_v2.jpg" alt="">
     </button>
   </nav>`
-  const logo = this.querySelector('#logo');
-  logo.addEventListener('click', this.homePage);
+    const logo = this.querySelector('#logo');
+    logo.addEventListener('click', this.homePage);
+
+    const btnMenuNav = this.querySelector('#btnMenuNav')
+    const divMenu = document.querySelector('.asideMenu')
+    const divPrincipal = document.querySelector('.main')
+
+    btnMenuNav.onclick = function () {
+
+      const dentroMenu = document.querySelector('#dentroMenu')
+
+      if (divPrincipal.classList.contains('col-9')) {
+        divPrincipal.classList.toggle('col-9')
+      } else {
+        divPrincipal.classList.toggle('col-12')
+      }
+      divMenu.classList.toggle('d-none')
+      dentroMenu.style.display = "block";
+      btnMenuNav.style.display = "none";
+
     }
-    homePage(){
-      location.href = './index.html';
-    }
+  }
+  homePage() {
+    location.href = './index.html';
+  }
 }
 customElements.define("barra-nav", BarraNav);
 
@@ -33,13 +53,12 @@ customElements.define("barra-nav", BarraNav);
 ---------------------------------------*/
 
 class MenuLateral extends HTMLElement {
-    connectedCallback() {
-        localStorage.setItem('tipoCatalogo', 0)
-        this.innerHTML =
-            `
+  connectedCallback() {
+    this.innerHTML =
+      `
         <div id="dentroMenu">
 
-          <div id="btnMenu"><span class="material-icons" style="margin-inline: 10px;">menu</span></div>
+          <div id="btnMenu"><span class="material-icons">menu</span></div>
 
           <h3><span class="material-icons" style="margin-inline: 10px;">rocket_launch</span>
             Lançamento</h3>
@@ -71,28 +90,46 @@ class MenuLateral extends HTMLElement {
 
           <h3><span class="material-icons" style="margin-inline: 10px;">videogame_asset</span>
             Jogos</h3>
-          <p data-tipo="17">Top 10</p>
-          <p data-tipo="18">Noticias</p>
-          <p data-tipo="19">Exclusivos para XBOX</p>
-          <p data-tipo="20">Exclusivos para PS</p>
-          <p data-tipo="21">Exclusivos para PC</p>
-          <p data-tipo="22">Campeonatos</p>
+          <p data-tipo="17">Noticias</p>
+          <p data-tipo="18">Exclusivos para XBOX</p>
+          <p data-tipo="19">Exclusivos para PS</p>
+          <p data-tipo="20">Exclusivos para PC</p>
+          <p data-tipo="21">Campeonatos</p>
 
           <h3><span class="material-icons" style="margin-inline: 10px;">info</span>Sobre</h3>
           <p>Historia do Site</p>
           <p>Direitos Reservados</p>
         </div>
       `;
-        const menuItems = this.querySelectorAll('p')
-        menuItems.forEach(item => {
-            item.addEventListener('click', this.handleClick)
-        });
+
+
+    const menuItems = this.querySelectorAll('p')
+    menuItems.forEach(item => {
+      item.addEventListener('click', (event) => this.handleClick(event))
+    });
+
+    const btnMenu = this.querySelector('#btnMenu')
+    const btnMenuNav = document.querySelector('#btnMenuNav')
+    const dentroMenu = this.querySelector('#dentroMenu')
+    const divMenu = document.querySelector('.asideMenu')
+    const divPrincipal = document.querySelector('.main')
+
+    btnMenu.onclick = function () {
+      if (divMenu.classList.contains('col-2')) {
+        dentroMenu.style.display = "none";
+        btnMenuNav.style.display = "block";
+        divMenu.classList.toggle('d-none')
+        if (divPrincipal.classList.contains('col-10')) {
+          divPrincipal.classList.toggle('col-12')
+        } else {
+          divPrincipal.classList.toggle('col-9')
+        }
+      }
     }
-    handleClick(event) {
-        const catalogo = event.target.getAttribute('data-tipo');
-        localStorage.setItem('tipoCatalogo', catalogo)
-        console.log(catalogo)
-        location.href = './telaDePost.html';
-    }
+  }
+  handleClick(event) {
+    const catalogo = event.target.getAttribute('data-tipo');
+    location.href = `./telaDePost${catalogo}.html`;
+  }
 }
 customElements.define("menu-lateral", MenuLateral)
